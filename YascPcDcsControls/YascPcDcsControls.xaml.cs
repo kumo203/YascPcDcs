37dca5f14a0410a4792d0088e01e3890abfa1ba4
山中知也 <tomoya.yamanaka@yasc.info>
2019-05-16 20:40:23 +0800

    Update control properties

--- YascPcDcsControls/YascPcDcsControls.xaml.cs
+++ YascPcDcsControls/YascPcDcsControls.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,59 @@ namespace YascPcDcsControls
     /// </summary>
     public partial class PcDcsControl : UserControl
     {
+        readonly string separator = ".";
+
         public PcDcsControl()
         {
             InitializeComponent();
         }
+
+        [Category("Opc")]
+        [Description("OPC Server Name")]
+        public string OpcServerName
+        {
+            get { return (string)GetValue(OpcServerNameProperty); }
+            set { SetValue(OpcServerNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty OpcServerNameProperty =
+            DependencyProperty.Register("OpcServerName", typeof(string), typeof(PcDcsControl), new PropertyMetadata(""));
+
+
+        [Category("Opc")]
+        [Description("OPC Register Name")]
+        public string OpcRegisterName
+        {
+            get { return (string)GetValue(OpcRegisterNameProperty); }
+            set { SetValue(OpcRegisterNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty OpcRegisterNameProperty =
+            DependencyProperty.Register("OpcRegisterName", typeof(string), typeof(PcDcsControl), new PropertyMetadata(""));
+
+        public string OpcName
+        {
+            get => OpcServerName + separator + OpcRegisterName;
+        }
+
+        [Category("Opc")]
+        [Description("OPC Analog Value")]
+        public int OpcAnalogValue
+        {
+            get { return (int)GetValue(OpcAnalogValueProperty); }
+            set { SetValue(OpcAnalogValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for OpcAnalogValue.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OpcAnalogValueProperty =
+            DependencyProperty.Register("OpcAnalogValue", typeof(int), typeof(PcDcsControl), new PropertyMetadata(0, ValueChanged));
+
+        static private void ValueChanged(DependencyObject target,
+            DependencyPropertyChangedEventArgs e)
+        {
+            var ths = (target as PcDcsControl);
+            ths.TextBlock.Text = ths.OpcAnalogValue.ToString();
+        }
+
     }
 }
