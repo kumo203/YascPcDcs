08b0e149e5ddfaa350b3fc1d0fe9be6efed20cf6
山中知也 <tomoya.yamanaka@yasc.info>
2019-05-28 02:31:32 +0800

    Adding PcDcsBase Class

---
+++ YascPcDcsControls/PcDcsBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace YascPcDcsControls
+{
+    public class PcDcsBase : UserControl
+    {
+    }
+
+    public class PcDcsAnalog : PcDcsBase
+    {
+    }
+
+    public class PcDcsDigital : PcDcsBase
+    {
+    }
+}
